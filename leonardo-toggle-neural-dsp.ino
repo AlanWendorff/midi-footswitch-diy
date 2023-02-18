d35e310139b62e6bfc8c356a7b50ce20158504a5
Alan Wendorff <alanwff4@gmail.com>
2023-02-18 12:46:35 -0300

    created new functions

--- leonardo-toggle-neural-dsp.ino
+++ leonardo-toggle-neural-dsp.ino
@@ -4,27 +4,24 @@
 const int N_BUTTONS = 8;
 const int BUTTONS[N_BUTTONS] = {2, 3, 4, 5, 6, 7, 8, 9};
 
-// Button states
-int buttonCurrentState[N_BUTTONS] = {};       
-int buttonPrevState[N_BUTTONS] = {};  
-boolean buttonStates[N_BUTTONS] = {false, false, false, false, false, false, false, false}; 
+// BUTTONS STATES
+int btnCurrentState[N_BUTTONS] = {};       
+int btnPrevState[N_BUTTONS] = {};  
+boolean btnVirtualRetention[N_BUTTONS] = {false, false, false, false, false, false, false, false}; 
 
 // LEDS
 const int N_LEDS = 8;
 const int LEDS[N_LEDS] = {A0, A1, A2, A3, A4, A5, 12, 11};
 
-// debounce
+// BUTTON DEBOUNCE
 unsigned long lastDebounceTime[N_BUTTONS] = {0};
 unsigned long debounceDelay = 50;  
 
 // MIDI
-const byte MIDI_CHANNEL = 0; //* MIDI channel to be used
+const byte MIDI_CHANNEL = 0;
 const byte MIDI_NOTES[N_BUTTONS] = {0, 12, 24, 36, 48, 60, 72, 84};
-const byte MIDI_PROGRAMS[3] = {0, 1, 2};
 
 void setup() {
-  //Serial.begin(115200); 
-  
   for (int i = 0; i < N_BUTTONS; i++) {
     pinMode(BUTTONS[i], INPUT_PULLUP);
   }
@@ -35,41 +32,48 @@ void setup() {
 }
 
 void loop() {
-    for (int index = 0; index < N_BUTTONS; index++) {
-      buttonCurrentState[index] = digitalRead(BUTTONS[index]);
-      if ((millis() - lastDebounceTime[index]) > debounceDelay) {
-        if (buttonPrevState[index] != buttonCurrentState[index]) {
-          lastDebounceTime[index] = millis();
-          if(buttonCurrentState[index] == LOW) {
-            //Serial.println(BUTTONS[i]);
-            if (index < 5) { 
-              handleNote(MIDI_CHANNEL, MIDI_NOTES[index], 64);
-              MidiUSB.flush();  
-              handleLedStatus(index);
-            }else {
-              handleProgramChange(MIDI_CHANNEL, index);
-              MidiUSB.flush();  
-              handleLedStatusPC(index);
-            }
-          }
-          buttonPrevState[index] = buttonCurrentState[index];
+  for (int index = 0; index < N_BUTTONS; index++) {
+    btnCurrentState[index] = digitalRead(BUTTONS[index]);
+    if ((millis() - lastDebounceTime[index]) > debounceDelay) {
+      if (btnPrevState[index] != btnCurrentState[index]) {
+        handleUpdateDeboundeTime(index);
+        handleFootCommand(index);
+        btnPrevState[index] = btnCurrentState[index];
       }
     }
   }
 }
 
+void handleFootCommand(int index) {
+  if(btnCurrentState[index] == LOW) {
+    if (index < 5) { 
+      handleNote(MIDI_CHANNEL, MIDI_NOTES[index], 64);
+      MidiUSB.flush();
+      handleLedLatching(index);
+    }else {
+      handleProgramChange(MIDI_CHANNEL, index);
+      MidiUSB.flush();
+      handleLedBlink(index);
+    }
+  }
+}
 
-void handleLedStatus(int index) {
-  if (buttonStates[index] == false){ 
-    buttonStates[index] = true;
+void handleUpdateDeboundeTime(int index) {
+  lastDebounceTime[index] = millis();
+}
+
+
+void handleLedLatching(int index) {
+  if (btnVirtualRetention[index] == false){ 
+    btnVirtualRetention[index] = true;
     digitalWrite(LEDS[index], HIGH);
   }else {
-    buttonStates[index] = false;
+    btnVirtualRetention[index] = false;
     digitalWrite(LEDS[index], LOW);
   }
 }
 
-void handleLedStatusPC(int index) {
+void handleLedBlink(int index) {
   digitalWrite(LEDS[index], HIGH);
   delay(100);
   digitalWrite(LEDS[index], LOW);
